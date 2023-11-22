de076df623f72b63e219e586412793f6a9132239
dlp2kor <SharanyaJayarama.Upadhyaya@in.bosch.com>
2023-11-22 13:54:02 +0530

    changes again5

--- cypress.config.js
+++ cypress.config.js
@@ -1,36 +1,49 @@
-const cucumber = require('cypress-cucumber-preprocessor').default
+const cucumber = require('cypress-cucumber-preprocessor').default;
 const { defineConfig } = require("cypress");
 const { default: preprocessor } = require('cypress-cucumber-preprocessor');
 const report = require('multiple-cucumber-html-reporter');
 
 module.exports = defineConfig({
   e2e: { 
-    testIsolation:false,
-    pageLoadTimeout:80000,
+    testIsolation: false,
+    pageLoadTimeout: 80000,
     setupNodeEvents(on, config) {
       // implement node event listeners here
-      on('file:preprocessor', cucumber())
-      on('after:run', () => {
-        const browser = process.env.BROWSER || 'Unknown';
-    report.generate({
-      metadata: {
-        browser: {
-          name: 'Browser',
-          value: browser
-        },
-      },
-        jsonDir: 'cypress/reports/cucumber-json',
-        reportPath: 'cypress/reports'
-        // Add more metadata as needed
-    });
-
-    });
-  },
+      on('file:preprocessor', cucumber());
+      on('after:run', (results) => {
+        try {
+          if (results && results.totalFailed > 0) {
+            // Get the browser parameter from the environment
+            const browser = process.env.BROWSER || 'Unknown';
+    
+            // Generate the HTML report after the test run
+            report.generate({
+              // Configuration options, customize as needed
+              // ...
+              metadata: {
+                browser: {
+                  name: 'Browser',
+                  value: browser,
+                },
+                // Add more metadata as needed
+              },
+            });
+          } else {
+            console.log('No failed tests. Skipping report generation.');
+          }
+        } catch (error) {
+          console.error('Error during report generation:', error);
+        }
+      }); // Closing parenthesis for on('after:run'
+      jsonDir: 'cypress/reports/cucumber-json';
+      reportPath: 'cypress/reports'
+      // Add more metadata as needed
+    },
   specPattern: "cypress/e2e/*.feature"
-},
-    env: {
-      "servicesCategories": ["APIs","SaaS","PaaS","IaaS"],
-      "workflowCategories":["Pre Defined Workflow","Custom Workflow"],
-      "frameworkCategories":["Methods"]
-    }
-  })
+  },
+  env: {
+    "servicesCategories": ["APIs", "SaaS", "PaaS", "IaaS"],
+    "workflowCategories": ["Pre Defined Workflow", "Custom Workflow"],
+    "frameworkCategories": ["Methods"]
+  }
+});
